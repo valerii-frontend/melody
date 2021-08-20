5aefa5734201c59aa0b93a1e139d32825be9b0e7
Valerii from earth <62751717+valerii-frontend@users.noreply.github.com>
2021-08-20 14:42:54 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -41,3 +41,60 @@ downBtnEl.addEventListener("click", function (e) {
 		document.querySelector(`[data-floor='${currentFloor}']`).classList.add("current");
 	}
 });
+
+const btn = document.querySelector(".main__btn");
+const modal = document.querySelector(".modal");
+const modalClose = document.querySelector(".modal__close");
+const modalNumber = document.querySelector(".modal__number");
+const rooms = document.querySelectorAll(".modal__rooms path");
+const roomsList = document.querySelectorAll(".modal__list li");
+
+btn.addEventListener("click", function (e) {
+	if (modal) {
+		modal.classList.add("active");
+		modalNumber.textContent = currentFloorEl.textContent;
+	}
+});
+modalClose.addEventListener("click", function (e) {
+	if (modal) {
+		modal.classList.remove("active");
+		rooms.forEach((el) => {
+			el.classList.remove("current-room");
+		});
+		roomsList.forEach((element) => {
+			element.classList.remove("active");
+		});
+	}
+});
+
+rooms.forEach((room, i) => {
+	room.addEventListener("mouseover", function (e) {
+		rooms.forEach((el) => {
+			el.classList.remove("current-room");
+		});
+		roomsList.forEach((element) => {
+			element.classList.remove("active");
+		});
+		room.classList.add("current-room");
+		roomsList[9 - i].classList.add("active");
+	});
+	room.addEventListener("click", function (e) {
+		rooms.forEach((element) => {
+			element.classList.remove("current-room");
+		});
+		room.classList.add("current-room");
+	});
+});
+
+roomsList.forEach((element, i) => {
+	element.addEventListener("mouseover", function (e) {
+		roomsList.forEach((li) => {
+			li.classList.remove("active");
+		});
+		rooms.forEach((room) => {
+			room.classList.remove("current-room");
+		});
+		rooms[9 - i].classList.add("current-room");
+		element.classList.add("active");
+	});
+});
